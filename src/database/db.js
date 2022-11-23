b5b57a8a9459e341bf55fd76bf98383d15dc343a
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-11-23 16:20:41 -0300

    fix: dotenv install

--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,18 @@
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 try {
-  await mongoClient.connect();
-  console.log("MONGODB connected!");
+	await mongoClient.connect();
+	console.log('MONGODB connected!');
 } catch (err) {
-  console.log(err);
+	console.log(err);
 }
 
-const db = mongoClient.db("buffstoreAPI");
+const db = mongoClient.db('buffstoreAPI');
+
+const usersCollection = db.collection('users');
+const productsCollection = db.collection('products');
+const cartCollection = db.collection('cart');
