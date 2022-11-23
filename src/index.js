77926dee9f95eb44ce10a525bf9b21b0992ff0d8
Melissa <melmihomel@gmail.com>
2022-11-22 22:42:54 -0300

    feat: initial configs

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(5000, () => console.log("Port 5000"));
