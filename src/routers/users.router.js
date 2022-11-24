a13f44a93143e1cf61ccedd6ca531bf3826c0e86
Melissa <melmihomel@gmail.com>
2022-11-24 20:56:56 -0300

    feat: delete session of user

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signIn, signUp } from "../controllers/users.controller.js";
+import { signIn, signOut, signUp } from "../controllers/users.controller.js";
 import { signInBodyValidation } from "../middlewares/signInBodyValidation.middleware.js";
 import { signUpBodyValidation } from "../middlewares/signUpBodyValidation.middleware.js";
 
@@ -7,5 +7,6 @@ const router = Router();
 
 router.post("/sign-up", signUpBodyValidation, signUp);
 router.post("/sign-in", signInBodyValidation, signIn);
+router.delete("/sign-out/:sessionUserID", signOut)
 
 export default router;
