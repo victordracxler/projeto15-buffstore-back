5398d8321755589da37c0ce22af35a2c8339e0ba
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-11-25 23:32:52 -0300

    feat: Add items to cart

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
 	addProduct,
+	addToCart,
 	getAllProducts,
 	getOneProduct,
 } from '../controllers/products.controller.js';
@@ -12,5 +13,7 @@ router.post('/add-product', addProduct);
 router.get('/products', getAllProducts);
 
 router.get('/products/:id', getOneProduct);
+
+router.post('/addtocart', addToCart);
 
 export default router;
